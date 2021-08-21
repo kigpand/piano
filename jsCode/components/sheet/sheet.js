5bec52460cdf87bb38ebd575c9d747596ac050f3
kigpand <kigpand@naver.com>
2021-08-22 00:12:06 +0900

    img slider 미리보기 구현

--- jsCode/components/sheet/sheet.js
+++ jsCode/components/sheet/sheet.js
@@ -14,10 +14,39 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 import { BaseComponent } from './../components.js';
+var ImgArray = ['img/butterfly.png', 'img/airplane.jpg', 'img/littlestar.jpg'];
+var defaultImgArray = function (container, imgArray) {
+    for (var i = 0; i < imgArray.length; i++) {
+        var img = document.createElement('img');
+        img.classList.add('sheet__image');
+        img.src = imgArray[i];
+        container.appendChild(img);
+    }
+};
 var Sheet = /** @class */ (function (_super) {
     __extends(Sheet, _super);
     function Sheet() {
-        return _super.call(this, "\n        <div class=\"sheet\">\n            <div class=\"sheet__container\"></div>\n            <input type=\"file\" class=\"sheet__fileLoader\" />\n        </div>") || this;
+        var _this = _super.call(this, "\n        <div class=\"sheet\">\n            <div class=\"sheet__fileInputer\">\uD30C\uC77C \uB123\uC744\uACF3</div>\n            <input type=\"file\" multiple class=\"sheet__fileLoader\" accept=\".jpg, .jpeg, .png\"/>\n            <div class=\"sheet__viewer\">\n                <div class=\"sheet__container\"></div>\n            </div>\n        </div>") || this;
+        var fileInputer = _this.element.querySelector(".sheet__fileInputer");
+        var fileLoader = _this.element.querySelector(".sheet__fileLoader");
+        var sheetContainer = _this.element.querySelector(".sheet__container");
+        defaultImgArray(sheetContainer, ImgArray);
+        fileInputer.addEventListener("click", function () {
+            fileLoader.click();
+        });
+        fileLoader.addEventListener("change", function (e) {
+            while (sheetContainer.hasChildNodes()) {
+                sheetContainer.removeChild(sheetContainer.firstChild);
+            }
+            var files = e.target.files;
+            for (var i = 0; i < files.length; i++) {
+                var img = document.createElement('img');
+                img.classList.add('sheet__image');
+                img.src = URL.createObjectURL(files[i]);
+                sheetContainer.appendChild(img);
+            }
+        });
+        return _this;
     }
     return Sheet;
 }(BaseComponent));
--- src/components/sheet/sheet.ts
+++ src/components/sheet/sheet.ts
@@ -1,11 +1,48 @@
 import { BaseComponent } from './../components.js';
 
+const ImgArray = ['img/butterfly.png','img/airplane.jpg', 'img/littlestar.jpg'];
+
+const defaultImgArray = (container: HTMLElement,imgArray:Array<string>): void =>{
+    
+    for(let i = 0;i<imgArray.length;i++){
+        const img = document.createElement('img');
+        img.classList.add('sheet__image');
+        img.src = imgArray[i];
+        container.appendChild(img);
+    }
+}
+
 export class Sheet extends BaseComponent<HTMLElement>{
     constructor(){
         super(`
         <div class="sheet">
-            <div class="sheet__container"></div>
-            <input type="file" class="sheet__fileLoader" />
+            <div class="sheet__fileInputer">파일 넣을곳</div>
+            <input type="file" multiple class="sheet__fileLoader" accept=".jpg, .jpeg, .png"/>
+            <div class="sheet__viewer">
+                <div class="sheet__container"></div>
+            </div>
         </div>`)
+
+        const fileInputer = this.element.querySelector(".sheet__fileInputer")! as HTMLElement;
+        const fileLoader = this.element.querySelector(".sheet__fileLoader")! as HTMLElement;
+        const sheetContainer = this.element.querySelector(".sheet__container")! as HTMLElement;
+        defaultImgArray(sheetContainer, ImgArray);
+        fileInputer.addEventListener("click",()=>{
+            fileLoader.click();
+        })
+
+        fileLoader.addEventListener("change", (e: Event) =>{
+            while(sheetContainer.hasChildNodes()){
+                sheetContainer.removeChild(sheetContainer.firstChild! as HTMLElement);
+            }
+            const files = (<HTMLInputElement>e.target).files! as FileList;
+            for(let i = 0; i<files.length; i++){
+                const img = document.createElement('img');
+                img.classList.add('sheet__image');
+                img.src = URL.createObjectURL(files[i]);
+                sheetContainer.appendChild(img);
+            }
+        })
+        
     }
 }
